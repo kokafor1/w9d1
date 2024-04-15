0bb0a66bb1c59c5de33c0205a6aea6e697393787
kokafor1 <kennedyokafor2003@gmail.com>
2024-04-15 15:40:21 -0400

    added key

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
 export default function App(){
-  return <h1>Hello World</h1>
+  const firstName: string = 'Ken';
+  const lastName: string = 'Okafor';
+  const isLoggedIn : boolean  = true;
+  const posts: {id:number, title: string}[]= [
+    {id: 1, title: 'Yooo'},
+    {id: 2 , title: 'GLazzze'},
+    {id: 3 , title: 'jcngknkf'}
+  ]
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <h2>{isLoggedIn? `Welcome back ${firstName} ${lastName}` : 'Please log in or Sign Up'} </h2>
+      {posts.map(p => <h4 key={p.id}>{p.title}</h4>)}
+    </div>
+  )
 }
