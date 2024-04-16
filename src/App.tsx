ddbbb852c5bad825a39a6cfcfff20b01c935cca9
kokafor1 <kennedyokafor2003@gmail.com>
2024-04-16 16:39:26 -0400

    newer

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import Container from 'react-bootstrap/Container';
+import Home from './views/Home';
+import SignUp from './views/SignUp';
+
+
+
 export default function App(){
-  const firstName: string = 'Ken';
-  const lastName: string = 'Okafor';
-  const isLoggedIn : boolean  = true;
-  const posts: {id:number, title: string}[]= [
-    {id: 1, title: 'Yooo'},
-    {id: 2 , title: 'GLazzze'},
-    {id: 3 , title: 'jcngknkf'}
-  ]
-  return (
-    <div>
-      <h1>Hello World</h1>
-      <h2>{isLoggedIn? `Welcome back ${firstName} ${lastName}` : 'Please log in or Sign Up'} </h2>
-      {posts.map(p => <h4 key={p.id}>{p.title}</h4>)}
-    </div>
-  )
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    const handleClick = () => {
+        setIsLoggedIn(!isLoggedIn);
+    }
+
+    return (
+        <>
+            <Navigation isLoggedIn={isLoggedIn}/>
+            <Container>
+                <Routes>
+                    <Route  path='/' element={<Home isLoggedIn={isLoggedIn} handleClick={handleClick} />} />
+                    <Route path='/signup' element={<SignUp/>} />
+                </Routes>
+            </Container>
+        </>
+    )
 }
---
+++ src/types/index.ts
@@ -0,0 +1,16 @@
+export type UserType = {
+    id: number, 
+    firstName: string,
+    lastName: string,
+    username: string,
+    email: string,
+    dateCreated: string
+}
+
+export type PostType = {
+    id: number,
+    title: string,
+    body: string,
+    dateCreated: string,
+    author: UserType
+}
